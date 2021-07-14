af0b99fc7ee17f38281f098db727549c5ae789f0
Renaud17 <45036260+Renaud17@users.noreply.github.com>
2021-07-14 18:11:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,8 @@ def help_command(update: Update, _: CallbackContext) -> None:
 
 def run_bot(update: Update, _: CallbackContext) -> None:
     replica = update.message.text
-    answer = make_reply(msg)
+    #answer = make_reply(msg)
+    answer = bot_initialize(user_msg)
     update.message.reply_text(answer)
 
 
